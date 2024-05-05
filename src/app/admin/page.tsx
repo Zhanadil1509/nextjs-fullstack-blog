10f24b2e5d47116aef4b79592f7223f25dacb643
z.sadykov <sl8@@22oR8sl8@@22oR8>
2024-05-05 12:43:48 +0500

    was added pages

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,4 @@
+type Props = {};
+export default function AdminPage({}: Props) {
+  return <div>AdminPage</div>;
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,4 @@
+type Props = {};
+export default function BlogPage({}: Props) {
+  return <div>BlogPage</div>;
+}
